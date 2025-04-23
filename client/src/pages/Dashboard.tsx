70517c10501dde95e52aeff5ec1641a59d1d023d
maciejmaciek <hello@maciejglazer.com>
2025-04-23 22:52:40 +0200

    update

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -179,7 +179,7 @@ const Dashboard: React.FC = () => {
 
       {/* Footer */}
       <footer className="mt-auto text-center text-xs text-gray-500 pt-6 border-t">
-        <p>Supported by Mój Drugi Dom</p>
+        <p>Supported by Fundacja Drugi Dom</p>
         <img src="/drugi-dom-logo.png" alt="Mój Drugi Dom logo" className="w-12 h-auto mx-auto mt-2" />
       </footer>
     </div>
